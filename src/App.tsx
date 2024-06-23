cf9e67198753e7c4d40fc0b406370cc4021514c2
Bhangdongho <bhang97@naver.com>
2024-06-23 23:21:54 +0900

    11주차 4DAY 과제

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import "./App.css";
+import TodoList from "./Todolist";
+import Clock from "./Timer";
 
 function App() {
   let name = "리액트";
 
   return (
     <div className="container">
-      <h1 className="test">
-        Hello,
-        {name === "리액트" ? <h1>YES</h1> : null}!!
-      </h1>
-      <p>반갑습니다.</p>
+      <TodoList />
+      <Clock />
     </div>
   );
 }
